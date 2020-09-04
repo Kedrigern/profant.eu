658910d5ab36000f490b079c3e402419b4a84605
Ondřej Profant <ondrej.profant@gmail.com>
2020-09-04 11:49:32 +0200

    Uspechy.py

--- uspechy.py
+++ uspechy.py
@@ -3,16 +3,20 @@
 import io
 import pandas
 import requests
+from redminelib import Redmine, exceptions
 
 class Config:
+    verbose = True
+    resultPath = "./_data/media.csv"
     urls = {
+      "rm": "https://redmine.pirati.cz",
       "issue": "https://redmine.pirati.cz/issues/",
       "issues": "https://redmine.pirati.cz/projects/snemovna/issues.json",
       "issues_tags": "https://redmine.pirati.cz/issues.csv?utf8=✓&set_filter=1&sort=id%3Adesc&f%5B%5D=status_id&op%5Bstatus_id%5D=%3D&v%5Bstatus_id%5D%5B%5D=5&f%5B%5D=tracker_id&op%5Btracker_id%5D=%3D&v%5Btracker_id%5D%5B%5D=28&f%5B%5D=project_id&op%5Bproject_id%5D=%3D&v%5Bproject_id%5D%5B%5D=185&v%5Bproject_id%5D%5B%5D=186&v%5Bproject_id%5D%5B%5D=187&v%5Bproject_id%5D%5B%5D=188&v%5Bproject_id%5D%5B%5D=189&v%5Bproject_id%5D%5B%5D=190&v%5Bproject_id%5D%5B%5D=191&v%5Bproject_id%5D%5B%5D=192&v%5Bproject_id%5D%5B%5D=193&v%5Bproject_id%5D%5B%5D=125&v%5Bproject_id%5D%5B%5D=194&v%5Bproject_id%5D%5B%5D=195&v%5Bproject_id%5D%5B%5D=196&v%5Bproject_id%5D%5B%5D=197&f%5B%5D=&c%5B%5D=tags_relations&group_by=project&t%5B%5D="
     }
     file = "issues.csv"
-    verbose = True
-    resultPath = "./_data/media.csv"
+    tag = "profant"    
+    test_ids = [16472, 16226, 25212]
 
 def get_relevant_issues_id(tag=None, literal_eval=False):
   """
@@ -38,11 +42,37 @@ def get_relevant_issues_id(tag=None, literal_eval=False):
       names=['id','tag'] )
 
   if tag:
+    # TODO: case sensitive
     df = df[df['tag'].str.contains(tag)]
+
+  if Config.verbose:
+    print("DEBUG: get_relevant_issues_id with tag %s return %s issues" % (tag, df.id.count()))
+
   return df
 
+def get_relevant_issues():
+  """
+  https://python-redmine.com/resources/issue.html
+  """
+  df = get_relevant_issues_id(Config.tag)
+  redmine = Redmine(Config.urls['rm'])
+  issues = []
+  for i in df.id:
+    try:
+      issue = redmine.issue.get(int(i))
+      issues.append(issue)
+    except exceptions.ResourceNotFoundError:
+      if Config.verbose:
+        print("ERROR: Resource %s does not exists:" % (i))
+
+  if Config.verbose:
+    print("DEBUG: get_relevant_issues found %s issues" % len(issues))
+
+  return issues
+
 def test():
-  df3 = get_relevant_issues_id('Profant')
-  print(df3)
+  issues = get_relevant_issues()
+  print(issues[0].description)
+
 
 test()
